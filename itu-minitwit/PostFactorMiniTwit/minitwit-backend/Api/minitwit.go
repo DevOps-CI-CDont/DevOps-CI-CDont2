765d441646bbe2ecdce6bcc5c1e79e0d3a91e037
Jacelius <sipn@itu.dk>
2023-02-11 17:59:43 +0100

    feat: login endpoint

--- itu-minitwit/PostFactorMiniTwit/minitwit-backend/Api/minitwit.go
+++ itu-minitwit/PostFactorMiniTwit/minitwit-backend/Api/minitwit.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
@@ -15,8 +17,8 @@ func main() {
 	router.GET("/", getTimeline)
 	router.GET("/public", getPublicTimeline)
 	router.GET("/user/:username", getUsername)
-	router.POST("/follow", followUser)
-	router.POST("/unfollow", unfollowUser)
+	router.POST("/user/:username/follow", followUser)
+	router.POST("/user/:username/unfollow", unfollowUser)
 	router.POST("/post", postMessage)
 	router.POST("/login", login)
 	router.POST("/register", register)
@@ -61,16 +63,52 @@ func connect_db() error {
 	return nil
 }
 
-// endpoints
-func getTimeline(c *gin.Context) {
-	log.Println("getTimeline called")
-	//query database
-}
-
 func errorCheck(err error) {
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+// endpoints
+func getTimeline(c *gin.Context) {
+	connect_db()
+	//query database
+	// check cookie for session
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("user_id")
+		if err != nil {
+			// The cookie doesn't exist
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		userID := cookie.Value
+		rows, err := DB.Query(`select message.*, user.* from message, user
+	where message.flagged = 0 and message.author_id = user.user_id and (
+		user.user_id = ? or
+		user.user_id in (select whom_id from follower
+								where who_id = ?))
+	order by message.pub_date desc limit ?`, userID, userID, PER_PAGE)
+		messages := make([]Message, 0)
+		for rows.Next() {
+			msg := Message{}
+			user := User{}
+			err = rows.Scan(&msg.Message_id, &msg.Author_id, &msg.Text, &msg.Pub_date, &msg.Flagged, &user.User_id, &user.Username, &user.Email, &user.Pw_hash)
+			log.Println(msg)
+			msg.Author = user
+
+			messages = append(messages, msg)
+		}
+		log.Println("printing messages")
+		log.Println(messages)
+
+		errorCheck(err)
+
+		c.JSON(200, gin.H{"tweets": messages})
+
+		defer rows.Close()
+	})
+	http.ListenAndServe(":8080", nil)
+
 }
 
 func getPublicTimeline(c *gin.Context) {
@@ -104,8 +142,8 @@ func getPublicTimeline(c *gin.Context) {
 }
 
 func getUsername(c *gin.Context) {
-	log.Println("getUsername called")
-	// display user's tweet
+	name := c.Param("username")
+	c.String(http.StatusOK, "Hello %s", name)
 }
 
 func followUser(c *gin.Context) {
@@ -121,13 +159,29 @@ func postMessage(c *gin.Context) {
 }
 
 func login(c *gin.Context) {
-	log.Println("login called")
+	connect_db()
+
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+	//check if username and password are empty
+
+	connect_db()
+	userid := DB.QueryRow(`select user_id from user where user.Username = ? and user.pw_hash = ?`, username, password)
+
+	var userIdAsInt int
+	err := userid.Scan(&userIdAsInt)
+	errorCheck(err)
+
+	// set cookie
+	c.SetCookie("user_id", strconv.Itoa(userIdAsInt), 3600, "/", "localhost", false, false)
 }
 
-func register(c *gin.Context) {
+func register(c *gin.Context) 
+	
+
 	log.Println("register called")
 }
 
 func logout(c *gin.Context) {
-	log.Println("logout called")
+	c.SetCookie("user_id", "", -1, "/", "localhost", false, false)
 }
