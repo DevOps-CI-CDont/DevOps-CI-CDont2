1c35793fd97a1256b7a33c5a3750afbcd1fa24ab
Silas Paaske <sipn@itu.dk>
2023-02-12 11:55:12 +0100

    config: next config update

--- itu-minitwit/PostFactorMiniTwit/minitwit-frontend/next.config.js
+++ itu-minitwit/PostFactorMiniTwit/minitwit-frontend/next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  env: {
+    NEXT_PUBLIC_CORS_ORIGIN: "http://localhost:6001",
+    NEXT_PUBLIC_API_URL: "http://127.0.0.1:8080"
+  }
+}
+
+// module.exports = nextConfig
