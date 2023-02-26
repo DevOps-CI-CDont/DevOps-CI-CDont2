f888ce8f5bf80d1369227267640f8f9f37b6795c
Silas Paaske <sipn@itu.dk>
2023-02-26 16:10:58 +0100

    removed some options

--- itu-minitwit/minitwit-backend/Api/minitwit.go
+++ itu-minitwit/minitwit-backend/Api/minitwit.go
@@ -56,12 +56,6 @@ func DisableCors() gin.HandlerFunc {
         c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
         c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
         c.Header("Access-Control-Allow-Credentials", "true")
-
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(http.StatusNoContent)
-            return
-        }
-
         c.Next()
     }
 }
