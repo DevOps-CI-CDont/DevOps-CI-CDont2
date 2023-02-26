fd198bc6ed502fa524ad70e073f14577764a57de
Silas Paaske <sipn@itu.dk>
2023-02-26 15:49:05 +0100

    getpublictweets use proxy

--- itu-minitwit/minitwit-frontend/next.config.js
+++ itu-minitwit/minitwit-frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    // NEXT_PUBLIC_PROXY_URL: "http://138.68.93.147:3001",
+    NEXT_PUBLIC_PROXY_URL: "http://138.68.93.147:3001",
     NEXT_PUBLIC_API_URL: "http://138.68.93.147:8080"
   },
   output:'standalone'
--- itu-minitwit/minitwit-frontend/src/server/getPublicTweets.ts
+++ itu-minitwit/minitwit-frontend/src/server/getPublicTweets.ts
@@ -2,7 +2,7 @@ export async function getPublicTweets() {
   const PROXY_URL = process.env.NEXT_PUBLIC_PROXY_URL as string
   const API_URL = process.env.NEXT_PUBLIC_API_URL as string
   console.log("getPublicTweets fetching from " + API_URL + "/public")
-  const resp = await fetch(process.env.NEXT_PUBLIC_API_URL + "/public")
+  const resp = await fetch(PROXY_URL+ "/" + API_URL + "/public")
   console.log("getPublicTweets response: " + resp)
   const data = await resp.json()
   console.log("getPublicTweets data: " + data)
