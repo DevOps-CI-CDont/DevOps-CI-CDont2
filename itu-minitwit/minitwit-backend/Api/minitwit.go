3946016d6d957732650ffde6c4ccfae7ff5905c3
Silas Paaske <sipn@itu.dk>
2023-02-26 16:05:23 +0100

    more cors disabling in go

--- itu-minitwit/minitwit-backend/Api/minitwit.go
+++ itu-minitwit/minitwit-backend/Api/minitwit.go
@@ -45,8 +45,25 @@ func Start() {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
+	Router.Use(DisableCors())
 
-	Router.Run("0.0.0.0:8080")
+	Router.Run(":8080")
+}
+
+func DisableCors() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Header("Access-Control-Allow-Origin", "*")
+        c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        c.Header("Access-Control-Allow-Credentials", "true")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(http.StatusNoContent)
+            return
+        }
+
+        c.Next()
+    }
 }
 
 // Capitalized names are public, lowercase are privat
