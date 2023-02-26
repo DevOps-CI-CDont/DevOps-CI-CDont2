8ff1a13e8c361b7112d977896e559f2957146547
Silas Paaske <sipn@itu.dk>
2023-02-26 15:44:30 +0100

    api url

--- itu-minitwit/minitwit-frontend/next.config.js
+++ itu-minitwit/minitwit-frontend/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_PROXY_URL: "http://localhost:3001",
-    NEXT_PUBLIC_API_URL: "138.68.93.147:8080"
+    // NEXT_PUBLIC_PROXY_URL: "http://138.68.93.147:3001",
+    NEXT_PUBLIC_API_URL: "http://138.68.93.147:8080"
   },
   output:'standalone'
 }
