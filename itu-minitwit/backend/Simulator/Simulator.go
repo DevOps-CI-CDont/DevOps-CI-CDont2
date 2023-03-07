72f36ee17f518e2a4e6c777241d4af9beaaa3ab4
Silas Paaske <sipn@itu.dk>
2023-03-07 13:27:45 +0100

    removed a simulator print

--- itu-minitwit/backend/Simulator/Simulator.go
+++ itu-minitwit/backend/Simulator/Simulator.go
@@ -221,7 +221,7 @@ func msgsPerUser(c *gin.Context) {
 					VALUES (?, ?, ?, 0)`
 
 		main.DB.Exec(query, main.GetUserIdByName(c.Param("username")), body["content"], time.Now().Unix())
-		fmt.Println("DB Insertion completed!")
+		// fmt.Println("DB Insertion completed!")
 		// select the last inserted message
 		query = `SELECT message.*, user.* FROM message, user `
 
