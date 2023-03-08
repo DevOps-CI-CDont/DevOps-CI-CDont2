16ad6d13f06546a98d88a4c443b2f1b99367825e
kjaerb <kjaerb@gmail.com>
2023-03-08 08:55:48 +0100

    fix: users are able to follow/unfollow again

--- itu-minitwit/frontend/next.config.js
+++ itu-minitwit/frontend/next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_PROXY_URL: "http://138.68.93.147:3001",
-    NEXT_PUBLIC_API_URL: "http://138.68.93.147:8080"
+    NEXT_PUBLIC_API_URL: "http://138.68.93.147:8080",
   },
-  output:'standalone'
-}
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
