7cb2b0b034655a339bc92350fd13b6d357c1d034
Silas Paaske <sipn@itu.dk>
2023-03-15 12:41:46 +0100

    fix: tweet type

--- itu-minitwit/frontend/src/components/Message/Tweet.tsx
+++ itu-minitwit/frontend/src/components/Message/Tweet.tsx
@@ -5,7 +5,7 @@ import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 
-export function Tweet({ text, author_name, pub_date }: Tweet) {
+export function Tweet({ text, author_name, pub_date }: TweetType) {
   return (
     <div className='mx-4 my-4 py-2 px-2 bg-blue-500 flex rounded-md'>
       <div>
