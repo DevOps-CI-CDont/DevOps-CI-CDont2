9c43f9a8a35a7d4b41fd61a7ff7a067de5d0882a
Silas Paaske <sipn@itu.dk>
2023-03-22 13:11:59 +0100

    tried to add env variable to frontend container (didnt work)

--- itu-minitwit/frontend/next.config.js
+++ itu-minitwit/frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: "http://138.68.93.147:8080",
+    NEXT_PUBLIC_API_URL: "http://138.68.93.147:8080", // TODO: FIX TO USE ENV VARIABLE
   },
   output: "standalone",
   typescript: {
