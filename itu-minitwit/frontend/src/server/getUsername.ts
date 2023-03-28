2a10e6a1852fc821539d2028bb3e0d6ca04d4530
kjaerb <kjaerb@gmail.com>
2023-03-28 23:14:43 +0200

    fix: username shows, weird change in previous commit

--- itu-minitwit/frontend/src/server/getUsername.ts
+++ itu-minitwit/frontend/src/server/getUsername.ts
@@ -1,11 +1,5 @@
-import axios from "axios";
-
 export async function getUsernameById(userId: string | number) {
-	return await axios
-		.get(
-			`${
-				process.env.NEXT_PUBLIC_API_URL
-			}/getUserNameById?id=${userId.toString()}`
-		)
-		.then((response) => response.data);
+	return await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/getUserNameById?id=${userId.toString()}`
+	).then((response) => response.json());
 }
