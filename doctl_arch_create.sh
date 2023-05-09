76e5e15d3fb3cc3b6ad24018fb533776cf6979ce
Silas Paaske <sipn@itu.dk>
2023-05-09 15:33:26 +0200

    architecture creation script progress
    Co-authored-by: Oliver Holmberg Jørgensen <EraxterCodes@users.noreply.github.com>
    Co-authored-by: Benjamin Salo Kjær <kjaerb@users.noreply.github.com>

--- doctl_arch_create.sh
+++ doctl_arch_create.sh
@@ -9,8 +9,13 @@ fi
 ssh_key_fingerprints="ea:e6:b9:88:5a:8f:d6:4b:c7:03:06:c2:fd:aa:ca:a8,3d:83:40:84:6a:ee:3b:e4:da:e6:b8:c9:1e:4f:24:e5,58:15:a0:48:24:47:dc:79:af:80:1b:f9:e3:0e:ff:67,cd:82:77:0a:e5:c3:b4:b2:06:91:30:b6:b3:60:f0:76"
 echo "ssh_key_fingerprints: $ssh_key_fingerprints"
 
-# read path to a .env file
-read -p "Enter path to a .env file: " env_file_path
+env_file_path="itu-minitwit/backend/.env"
+
+# check if file exists
+if [ ! -f "$env_file_path" ]; then
+    echo "Error: File $env_file_path does not exist."
+    exit 1
+fi
 
 # create droplets
 echo "creating droplets"
@@ -20,9 +25,49 @@ doctl compute droplet create --image ubuntu-22-10-x64 --size s-1vcpu-2gb --regio
 
 # wait for droplets to be created
 echo "waiting 60 seconds for droplets to be created"
-sleep 60
+sleep 10
+echo "50"
+sleep 10
+echo "40"
+sleep 10
+echo "30"
+sleep 10
+echo "20"
+sleep 10
+echo "10"
+sleep 10
+echo "Done waiting"
 
-# install docker engine on manager1
+
+# get droplet IP addresses
+manager1_ip=$(doctl compute droplet get manager1 --format PublicIPv4 --no-header)
+wanky=$(doctl compute droplet get worker1 --format PublicIPv4 --no-header)
+worker2_ip=$(doctl compute droplet get worker2 --format PublicIPv4 --no-header)
+
+# print IP addresses
+echo "Manager1 IP address: $manager1_ip"
+echo "Worker1 IP address: $wanky"
+echo "Worker2 IP address: $worker2_ip"
+
+# add droplets to known hosts
+sleep 1
+ssh-keyscan -H $manager1_ip >> ~/.ssh/known_hosts
+sleep 1
+ssh-keyscan -H $wanky >> ~/.ssh/known_hosts
+sleep 1
+ssh-keyscan -H $worker2_ip >> ~/.ssh/known_hosts
+
+# Check that all dropslets are added to known hosts otherwise run again
+# add droplets to known hosts
+for ip in $manager1_ip $wanky $worker2_ip; do
+    while ! ssh-keygen -F $ip | grep -q $ip; do
+        echo "Adding $ip to known hosts failed...retrying"
+        sleep 5
+        ssh-keyscan -H $ip >> ~/.ssh/known_hosts
+    done
+done
+
+# install docker engine on droplets
 echo "trying to install docker engine on manager1"
 doctl compute ssh manager1 --ssh-command "sudo snap install docker"
 echo "docker engine installed on manager1"
@@ -33,7 +78,24 @@ echo "trying to install docker engine on worker2"
 doctl compute ssh worker2 --ssh-command "sudo snap install docker"
 echo "docker engine installed on worker2"
 
-# transfer .env file to manager1
+# get tokens from .env file
+echo "trying to get tokens from .env file"
+source "$env_file_path"
+echo "manager_token: $manager_token"
+echo "worker1_token: $worker1_token"
+echo "worker2_token: $worker2_token"
+
+
+
+echo "installing doctl on droplet"
+doctl compute ssh manager1 --ssh-command "sudo snap install doctl"
+# swap to root on manager 1
+echo "swapping to root on manager1"
+doctl compute ssh manager1 --ssh-command "sudo -i"
+echo "manager1 doctl auth init with token"
+doctl compute ssh manager1 --ssh-command "sudo doctl auth init -t $manager_token"
+echo "manager1 doctl registry login"
+doctl compute ssh manager1 --ssh-command "sudo doctl registry login"
 
 # get docker compose file on manager 1
 echo "trying to get docker compose file on manager1"
