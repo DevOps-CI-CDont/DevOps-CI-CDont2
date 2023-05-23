9e01fcb06d46d8e990b911697aa596dd4f97b965
Ojoe <ojoe@itu.dk>
2023-05-23 16:15:06 +0200

    fix: more links to old ip removed

--- doctl_delete_droplets.sh
+++ doctl_delete_droplets.sh
@@ -2,4 +2,8 @@
 echo "deleting manager1"
 doctl compute droplet delete manager1 --force
 # remove domain record pointing to manager1
-doctl compute domain records delete cicdont.live 1 --force
+# Get all DNS A records for the domain
+records=$(doctl compute domain records list "cicdont.live" --format "ID,Type" --no-header | grep "A" | awk '{print $1}')
+for record_id in $records; do
+  doctl compute domain records delete "cicdont.live" "$record_id"
+done
